afc33dd8cf2bb619478f94adc4e77b6a4d204ef1
tbskgm <yxiaoju01@gmail.com>
2023-05-05 17:15:51 +0900

    add: First commit.

---
+++ isowardsMultiProject/isowardsMultiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  isowardsMultiProjectApp.swift
+//  isowardsMultiProject
+//
+//  Created by 小駒翼 on 2023/05/05.
+//
+
+import SwiftUI
+
+@main
+struct isowardsMultiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
